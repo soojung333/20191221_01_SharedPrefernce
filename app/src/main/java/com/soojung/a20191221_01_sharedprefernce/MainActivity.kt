828cc0d8d1b6d047e2d1295342066a7dda77fc18
soojung <blessme333@gmail.com>
2019-12-21 11:31:41 +0900

    ID 저장 체크박세 체크 여부도 함께 저장 / 반영

--- app/src/main/java/com/soojung/a20191221_01_sharedprefernce/MainActivity.kt
+++ app/src/main/java/com/soojung/a20191221_01_sharedprefernce/MainActivity.kt
@@ -2,6 +2,7 @@ package com.soojung.a20191221_01_sharedprefernce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.soojung.a20191221_01_sharedprefernce.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,7 +16,7 @@ class MainActivity : BaseActivity() {
         setValues()
 
         loginBtn.setOnClickListener {
-            if (idCeckBox.isChecked) {
+            if (idCheckBox.isChecked) {
                 val userId = idEdt.text.toString()
 
                 ContextUtil.setUserId(mContext, userId)
@@ -25,21 +26,36 @@ class MainActivity : BaseActivity() {
         }
 
 
+
     }
 
 
     override fun setupEvents() {
 
 //        Q1. 아이디 저장 로직 구현
-//        loginBtn이 눌리면
-//        idCheckBox가 체크되어 있는지 검사
-//        되어 있다면 edEdt에 적혀 있는 값을 userId 라는 val로 저장
+//        loginBtn 이 눌리면
+//        idCheckBox 가 체크되어 있는지 검사
+//        되어 있다면 edEdt 에 적혀 있는 값을 userId 라는 val로 저장
 
+
+//        Q2. 아이디 저장 체크박스 체크여부 (boolean) 저장
+//        idCheckBox 가 체크 되면 (구글링)
+//        체크 된 값 (체크/해제)을 ContextUtil - SharePreference 활용해서 저장
+//        앱이 켜질 때 setValues 에서 저장된 체크 여부를 반영 (구글링)
+
+        idCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            ContextUtil.setUserIdSave(mContext, isChecked)
+
+        }
 
 
     }
 
     override fun setValues() {
+        idEdt.setText(ContextUtil.getUserId(mContext))
+
+        idCheckBox.isChecked = ContextUtil.getUserIdSave(mContext)
 
     }
 
--- app/src/main/java/com/soojung/a20191221_01_sharedprefernce/utils/ContextUtil.kt
+++ app/src/main/java/com/soojung/a20191221_01_sharedprefernce/utils/ContextUtil.kt
@@ -12,6 +12,7 @@ class ContextUtil {
 
         //    사용자의 ID를 [저장-set/로드-get]할 때 사용하는 항목의 이름을 상수로 저장.
         val USER_ID = "USER_ID"
+        val USER_ID_SAVE = "USER_ID_SAVE"
 
 //    userId를 저장하는 기능을 구현한 함수
         fun setUserId(context: Context, inputId: String) {
@@ -28,8 +29,22 @@ class ContextUtil {
         fun getUserId(context: Context) : String {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
-            return pref.getString(USER_ID, "")!!
+            return pref.getString(USER_ID, " ")!!
         }
+
+        fun setUserIdSave(context: Context, needSave:Boolean) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(USER_ID_SAVE, needSave).apply()
+        }
+
+        fun getUserIdSave(context: Context) : Boolean{
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(USER_ID_SAVE, false)
+
+        }
+
+
+
     }
 
 }
