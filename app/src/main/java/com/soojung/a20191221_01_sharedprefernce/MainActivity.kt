b8345eaf99ec9141957f1163dfe0441df6fee19b
soojung <blessme333@gmail.com>
2019-12-21 10:31:33 +0900

    Initial commit

---
+++ app/src/main/java/com/soojung/a20191221_01_sharedprefernce/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.soojung.a20191221_01_sharedprefernce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
